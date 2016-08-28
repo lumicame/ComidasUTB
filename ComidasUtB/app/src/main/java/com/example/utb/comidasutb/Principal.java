0f0b4597f6132aac187b09adb5f295127c8b4a45
lumicame <lumicame3@gmail.com>
2016-08-27 19:43:51 -0500

    a

---
+++ ComidasUtB/app/src/main/java/com/example/utb/comidasutb/Principal.java
@@ -0,0 +1,55 @@
+package com.example.utb.comidasutb;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        List items =new ArrayList();
+        items.add(new restaurantes("Alcatraz",R.mipmap.ic_launcher,"alguna descripcion"));
+        items.add(new restaurantes("otro",R.mipmap.ic_launcher,"alguna descripcion"));
+        items.add(new restaurantes("solo cafe",R.mipmap.ic_launcher,"alguna descripcion"));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_principal);
+        mRecyclerView.setHasFixedSize(true);
+
+        // Usar un administrador para LinearLayout
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+// Crear un nuevo adaptador
+        mAdapter = new MyAdapter(items);
+        mRecyclerView.setAdapter(mAdapter);
+        // use a linear layout manager
+
+    }
+
+}
+
